f90cc2ed314311f918d4a8a56256b596023841a4
1263919792@qq.com <1w2b3c4g>
2020-10-20 01:23:40 +0800

    sql

--- src/main/resources/sql/jpa.sql
+++ src/main/resources/sql/jpa.sql
@@ -11,31 +11,55 @@
  Target Server Version : 80016
  File Encoding         : 65001
 
- Date: 19/10/2020 00:59:29
+ Date: 20/10/2020 01:23:12
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
+-- Table structure for b_order0
+-- ----------------------------
+DROP TABLE IF EXISTS `b_order0`;
+CREATE TABLE `b_order0`  (
+  `id` bigint(20) NOT NULL,
+  `company_id` int(11) NULL DEFAULT NULL,
+  `position_id` int(11) NULL DEFAULT NULL,
+  `name` varchar(56) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for b_order1
+-- ----------------------------
+DROP TABLE IF EXISTS `b_order1`;
+CREATE TABLE `b_order1`  (
+  `id` bigint(20) NOT NULL,
+  `company_id` int(11) NULL DEFAULT NULL,
+  `position_id` int(11) NULL DEFAULT NULL,
+  `name` varchar(56) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
 -- Table structure for position
 -- ----------------------------
 DROP TABLE IF EXISTS `position`;
 CREATE TABLE `position`  (
-  `id` bigint(27) NOT NULL,
+  `id` bigint(31) NOT NULL,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `salary` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `city` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 524713706852974593 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for position_detail
 -- ----------------------------
 DROP TABLE IF EXISTS `position_detail`;
 CREATE TABLE `position_detail`  (
-  `id` int(11) NOT NULL,
-  `pid` int(11) NULL DEFAULT NULL,
+  `id` bigint(31) NOT NULL,
+  `pid` bigint(31) NULL DEFAULT NULL,
   `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
